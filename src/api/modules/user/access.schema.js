d611018fc85af047521d531ca91b407fc8b2ff73
saeedseng@gmail.com <saeedseng@gmail.com>
2025-08-02 20:04:20 +0330

    seeders problem

--- src/api/modules/user/access.schema.js
+++ src/api/modules/user/access.schema.js
@@ -1,19 +1,32 @@
 const { default: mongoose } = require("mongoose");
 
+// const AccessSchema = new mongoose.Schema(
+//   {
+//     type: {
+//       isBlocked: Boolean,
+//       isWrite: Boolean,
+//       isDelete: Boolean,
+//       isEdit: Boolean,
+//     },
+//     default: {
+//       isBlocked: false,
+//       isWrite: false,
+//       isDelete: false,
+//       isEdit: false,
+//     },
+//   },
+//   { timestamps: true, _id: true }
+// );
+
+// module.exports = AccessSchema;
+// const { default: mongoose } = require("mongoose");
+
 const AccessSchema = new mongoose.Schema(
   {
-    type: {
-      isBlocked: Boolean,
-      isWrite: Boolean,
-      isDelete: Boolean,
-      isEdit: Boolean,
-    },
-    default: {
-      isBlocked: false,
-      isWrite: false,
-      isDelete: false,
-      isEdit: false,
-    },
+    isBlocked: { type: Boolean, default: false },
+    isWrite: { type: Boolean, default: false },
+    isDelete: { type: Boolean, default: false },
+    isEdit: { type: Boolean, default: false },
   },
   { timestamps: true, _id: true }
 );
